b8c84434e5b359c76df5c115bc15c9f3014351d7
boai <sunboyan@outlook.com>
2017-01-11 14:02:10 +0800

    优化

---
+++ BAAnimation/BAAnimation/BAAnimation.h
@@ -0,0 +1,25 @@
+//
+//  BAAnimation.h
+//  BAAnimation
+//
+//  Created by 博爱 on 2017/1/11.
+//  Copyright © 2017年 DS-Team. All rights reserved.
+//
+
+#ifndef BAAnimation_h
+#define BAAnimation_h
+
+#pragma mark - UIView 动画
+#import "UIView+BAAnimation.h"
+
+#pragma mark - Transition 动画
+#import "UIView+BATransition.h"
+
+#pragma mark - 加载动画
+#import "BALoadingView.h"
+#import "BAActivityIndicatorView.h"
+#import "BALoadingWaveView.h"
+
+
+
+#endif /* BAAnimation_h */
